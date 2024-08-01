f468558cbdb2062bb8d98e59aa24304944c6fc1a
Nazariy2 <nazrud23@gmail.com>
2024-08-01 15:14:51 +0200

    a

---
+++ script.js
@@ -0,0 +1,26 @@
+// let result = 5 + 5 +'5';
+
+// console.log(result);
+// console.log(typeof result);
+
+// let email = prompt('write ur email');
+// let emailIncludes = email.includes('@');
+// let emailLength = email.length;
+
+// console.log(`ur email is ${email} and the length of ur email is ${emailLength}`);
+// console.log(`ur email must include @ = ${emailIncludes}`);
+
+// let word1 = 'My';
+// let word2 = 'name';
+// let word3 = 'is';
+
+// let fullName = word1 + ' ' + word2 + ' ' + word3 + ' ' + 'Victor';
+
+// console.log(fullName);
+
+function showAlert() {
+    let name = 'Nazariy';
+    let payment = 300
+
+    alert(`Дякуємо, ${name}! До сплати ${payment} гривень.`);
+}
